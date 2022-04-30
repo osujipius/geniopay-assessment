4f605e6ba7fb2f07c1c58c9b27dc55e8f9f08fd0
Pius Osuji <piusosuji9@gmail.com>
2022-05-01 05:31:35 +1200

    more features

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./table.css";
-import MockData from "./MOCK_DATA";
+import MockData from "./TABLE_DATA";
 
 export default function Table() {
   const [data, setdata] = useState(MockData);
@@ -33,13 +33,17 @@ export default function Table() {
             <tbody>
               {data.map((data) => (
                 <tr key={data.id}>
-                  <td style={{ display: "flex" }}>
-                    <img src={data.img} alt="" />
+                  <td data-label="Currency" style={{ display: "flex" }}>
+                    <img
+                      style={{ borderRadius: "50%" }}
+                      src={data.img}
+                      alt=""
+                    />
                     <p>{data.title}</p>
                   </td>
-                  <td>{data.rate}</td>
-                  <td>{data.change}</td>
-                  <td>
+                  <td data-label="Rate">{data.rate}</td>
+                  <td data-label="Change">{data.change}</td>
+                  <td data-label="Chart">
                     <img src={data.chart} alt="" />
                   </td>
                 </tr>
